c24d922321ada8d16b196139e2a2a7e7b58b6ab5
SensacionRadio <59749648+SensacionRadio@users.noreply.github.com>
2021-08-26 23:58:50 -0400

    Add files via upload

---
+++ proyect-josegilfortoul/db.php
@@ -0,0 +1,7 @@
+<?php
+	$conexion=mysqli_connect("localhost","josegilfortoul","12345","registro_inscripcion");
+
+	if (!$conexion){
+		echo "No conecta";
+	}
+?>
---
+++ proyect-josegilfortoul/registropreinscripcion.php
@@ -0,0 +1,36 @@
+<?php
+	
+	$banco=$_POST['banco'];
+	$num_referencia=$_POST['num_referencia'];
+	$tipo_pago=$_POST['tipo_pago'];
+	$fecha=$_POST['fecha'];
+	$archivo=$_POST['archivo'];
+
+	
+	
+
+	session_start();
+
+	$cedula=$_SESSION['ci_alumno'];
+
+	include('db.php');
+
+	$consulta="INSERT INTO datos_de_pago (id_alumno, banco, nro_referencia, tipo_pago, fecha, archivo) VALUES ('$cedula', '$banco', '$num_referencia', '$tipo_pago', '$fecha', '$archivo')";
+
+	$resultado=mysqli_query($conexion,$consulta);
+
+	if ($resultado) {
+	   
+		header("location:archivo_registrado_exitosamente.html");
+	    }
+
+	    else {
+	    	echo "falló el registro, la cédula ya existe";
+	    		   
+	    }  
+
+	// mysqli_free_result($resultado);
+	mysqli_close($conexion);
+	
+
+?>
